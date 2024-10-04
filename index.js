904b7b0aeba621542d044b6173c9985acb0a35d2
Thomas Lee Castro <thomas.castro@netlify.com>
2024-10-04 11:36:46 -0700

    Added contacts and update files

--- index.js
+++ index.js
@@ -1,70 +1,68 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const path = require ('path');
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const private_app_token = 'pat-na1-0f8a676b-24b2-46e3-afaf-6ddecca1abd3'
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/homepage-establishments', async (req, res) => {
+    const establishmentsEndpoint = 'https://api.hubspot.com/crm/v3/objects/establishments?properties=name,address,contact_person,email_address,establishment_type';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+      Authorization: `Bearer ${private_app_token}`,
+      'Content-Type': 'application/json'
+    }
+    const params = {
+      properties: ['name', 'address', 'contact_person' , 'email_address' , 'establishment_type']
     }
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+      const response = await axios.get(establishmentsEndpoint, { headers, params });
+      console.log('API Response:', JSON.stringify(response.data, null, 2));
+      const establishments = response.data.results;
+      console.log('Establishment data:', JSON.stringify(establishments, null, 2));
+      res.render('homepage', { establishments: establishments });
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
+  })
+  
+  app.get('/update-establishments', (req, res) => {
+    try {
+      res.render('updates', { pageTitle: 'Update Custom Object Form | Integrating With HubSpot I Practicum' }); 
+    } catch (error) {
+      console.error(error);
     }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+  });
+  
+  
+  app.post('/update-establishments', async (req, res) => {
+    const establishmentsEndpoint = 'https://api.hubspot.com/crm/v3/objects/establishments';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+      Authorization: `Bearer ${private_app_token}`,
+      'Content-Type': 'application/json'
     }
-
-});
-*/
+    const data = {
+      properties: {
+        name: req.body.name,
+        address: req.body.address,
+        contact_person: req.body.contact_person,
+        email_address: req.body.email_address,
+        establishment_type: req.body.establishment_type
+      }
+    }
+    try {
+      const response = await axios.post(establishmentsEndpoint, data, { headers });
+      console.log('API Response:', JSON.stringify(response.data, null, 2));
+      res.redirect('/homepage-establishments');
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
 
 // * Localhost
